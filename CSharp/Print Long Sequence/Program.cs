0a6a91522ab671e9c2c7f0957720719f5cba92df
DesislavIT <deso91@abv.bg>
2015-07-01 20:58:13 +0300

    CSharp

---
+++ CSharp/Print Long Sequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_a_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 1001; i++)
+            {
+                if (IsOdd(i))
+                {
+                    Console.Write(-i + ",");
+                }
+                else
+                {
+                    Console.Write(i + ",");
+                }
+            }
+        }
+        public static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+    }
+}
