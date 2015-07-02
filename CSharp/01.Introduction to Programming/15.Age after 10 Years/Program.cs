302b94b74a71375dd5eb825542da69ede8909392
DesislavIT <deso91@abv.bg>
2015-07-02 22:39:10 +0300

    C#

---
+++ CSharp/01.Introduction to Programming/15.Age after 10 Years/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age_after_10_Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime BirthDate = DateTime.Parse(Console.ReadLine());
+            int age = (int)((DateTime.Now - BirthDate).TotalDays / 365.242199);
+            Console.WriteLine("You are " + age + " years old.");
+            Console.WriteLine("After ten years you will be " + (age + 10) + "years old");
+        }
+    }
+}
